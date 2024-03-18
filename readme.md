c8bef9723c9bed234c37cca689f9129f7286aab2
George Schafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-03-18 05:30:13 -0700

    Initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Phaser3 Boilerplate
+
+This is a quick start template for PhaserJS.
+
+## Server
+
+I use the VSCode extension Live Server by Ritwick Dey. You can use any server you want.
+
+Have fun!
---
+++ test/index.spec.mjs
@@ -0,0 +1,29 @@
+import {
+    throwError,
+    did,
+    does,
+    have,
+    has,
+    is,
+    matches,
+    getCounter,
+    valueMatch,
+    objectsMatch,
+    throwsError,
+    constructorThrowsError,
+    nullCheck,
+    compileKeywords,
+    SchemaTypeValue,
+    SchemaTypeProperty,
+    schemaCorresponds
+} from './ChaiTests/Chai.mjs'
+
+
+
+
+
+describe('', () => {
+    describe('', () => {
+
+    })
+})
