288d4f93e5b363d32eaf26e3b1b0b512a455fc5c
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-03-18 07:03:59 -0700

    added workaround source

--- public/js/loadEnv.mjs
+++ public/js/loadEnv.mjs
@@ -1,3 +1,6 @@
+// This is a suggestion I found to try to work around the problem. It did not work. Source:
+// https://stackoverflow.com/questions/64620877/cant-use-dotenv-with-es6-modules#:~:text=//-,importing%20environmental%20variables,-import%20express%20from
+
 import dotenv from 'dotenv';
 dotenv.config()
 
