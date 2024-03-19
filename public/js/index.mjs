4c81529e2bc5247b410b07eedbc609cefa748c34
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-03-19 01:01:38 -0700

    removed scene class

--- public/js/index.mjs
+++ public/js/index.mjs
@@ -2,7 +2,7 @@ import { assets } from './env.js'
 import { gun_parts } from './json/gun_parts.js'
 import {
     Config as Configuration,
-    Scene,
+    // Scene,
     View,
     Player,
     Typeface,
@@ -17,25 +17,25 @@ let activeScene
 let sceneA
 let configure
 let game
-gun_parts.image = '../assets/gun_parts.png' + gun_parts.image
+gun_parts.image = assets + gun_parts.image
 const view = new View(512, 382)
 const gunPartImg = new Img('gunPart', gun_parts.image)
-const elements = {
-    gunParts: new SpriteSheet('gunParts', gun_parts.image, new Frame(32,64))
-}
+// const elements = {
+//     gunParts: new SpriteSheet('gunParts', gun_parts.image, new Frame(32,64))
+// }
 
 const player = null
 
 function preload() {
     activeScene = this
-    for(let [key] of Object.entries(elements)){
-        elements[key].preload(activeScene)
-    }
+    // for(let [key] of Object.entries(elements)){
+    //     elements[key].preload(activeScene)
+    // }
     gunPartImg.preload(activeScene)
 }
 
 function create() {
-    elements.gunParts.createImg(activeScene, view.center().x, view.center())
+    // elements.gunParts.createImg(activeScene, view.center().x, view.center())
     gunPartImg.create(activeScene, view.center().x, view.center().y)
 }
 
@@ -43,7 +43,12 @@ function update() {
 
 }
 
-sceneA = new Scene(preload, create, update)
+sceneA = {
+    preload: preload,
+    create: create,
+    update: update
+}
+
 configure = new Configuration(view, null, sceneA)
 game = new Phaser.Game(configure)
 
